c154c582732e00513165377b9e402ed7275cd0e8
Alvison Hunter Arnuero <alvison@gmail.com>
2020-12-28 23:00:44 -0600

    Added randPWD file update and String Manipulation file as well

--- randomPwdGenerator.py
+++ randomPwdGenerator.py
@@ -1,20 +1,44 @@
-# This function will generate a random string with a lenght based on user input number.
-# This can be useful for temporary passwords or even for some temporary login tokens.
-import random
+# This function will generate a random string with a lenght based
+# on user input number. This can be useful for temporary passwords
+# or even for some temporary login tokens.
+# Made with ❤️ in Python 3 by Alvison Hunter - December 28th, 2020
+from random import sample
 import time
-def fnRandomString():
-    print("░░░░░░░░░░░░░░░░ Password Generator Tool ░░░░░░░░░░░░░░░")
-    numCharacters = int(input("Insert the desired number of characters: "))
-    if numCharacters < 70:
-        baseString = list ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@*+&^%$#!")
-        strResult = "" 
-        generated_list = random.sample(baseString, numCharacters) 
-        for character in generated_list:  
-          strResult += character
-        print("Generating new password, please hold...")
-        time.sleep(2)
-        print("The new password is:\n {}".format(strResult))
-    else:
-        print ("The entered amount is too big, number has to be less than 70")
-    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
-fnRandomString()
+from datetime import datetime
+# We will incorporate some colors to the terminal
+class bcolors:
+    PRIMARY ='\033[34m'
+    SECONDARY = '\033[95m'
+    FOOTER = '\033[37m'
+    INFO = '\033[36m'
+    SUCCESS = '\033[32m'
+    WARNING = '\033[93m'
+    DANGER = '\033[31m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Main function is defined here
+def generate_rand_pwd():
+    try:
+        print("░░░░░░░░░░░░░ Hunter Password Generator Tool ░░░░░░░")
+        print("============= We do not store any passwords. =======")
+        numCharacters = int(input("Please Enter Password Length: \n"))
+        if numCharacters < 70:
+            baseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@*+&^%$#!"
+            strResult = ''.join(sample(baseString, numCharacters))
+            print(f"{bcolors.WARNING}Generating new password, please hold...{bcolors.ENDC}")
+            time.sleep(1)
+            print(f"{bcolors.INFO}The new password is:{bcolors.ENDC}\n {bcolors.SUCCESS}{strResult}{bcolors.ENDC}")
+        else:
+            print ("The password length is too big, number has to be less than 70")
+            return
+    except:
+        print('Uh Oh, something went wrong!')
+    finally:
+        print(f"{bcolors.FOOTER}© {datetime.today().strftime('%Y-%m-%d')} Hunter Password Generator Tool. All rights reserved.{bcolors.ENDC}")
+
+
+# Time to call the function now, fellows
+generate_rand_pwd()
--- stringManipulation.py
+++ stringManipulation.py
@@ -1,4 +1,5 @@
-# Given a string, return a new string where the first and last chars have been exchanged.
+# Given a string, return a new string where the first and last chars
+# have been exchanged.
 def front_back(str):
     if len(str) > 1:
       first = str[0]
